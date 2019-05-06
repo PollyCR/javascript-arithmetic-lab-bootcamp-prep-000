cd618e3f6fe82e69e4c62c0be6dec8d235fdf432
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 11:21:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var a = 1 
+var b = 80 
+function add(a,b) {
+return a + b }
+
+var c = 60
+var d = 40
+function subtract(c,d) {
+  return c - d 
+}
+
+var e = 2 
+var f = 3.4 
+function multiply(e,f) {
+  return e * f 
+}
+
+var g = 5.0 
+var h = 2.5 
+function divide (g,h) {
+  return g/h 
+}
+
+var i = 1 
+function inc (i) {
+  return (i)+1 
+}
+var j = 2
+function dec (j) {
+  return (j-1)
+}
